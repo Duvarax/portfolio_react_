015a7d7d862d539d3a0ed57ca9409ee17e0e55e9
Duvara Claudio <91039016+Duvarax@users.noreply.github.com>
2026-02-21 15:03:56 -0300

    -

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import person from "../assets/person.webp";
+import person from "../assets/person.png";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
